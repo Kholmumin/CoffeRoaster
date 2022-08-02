3e9bdc607c15fd793342dd762a123b09ef6340e4
Kholmumin <80518819+Kholmumin@users.noreply.github.com>
2022-08-02 05:39:05 +0500

    Added modal

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 let hamBtn = document.querySelector(".site-header__btn")
 let elHeader = document.querySelector(".site-header")
 
+
+
 hamBtn.addEventListener("click", function(){
     elHeader.classList.toggle("site-header__active")
+})
+
+
+
+var elmOpen = document.querySelector(".plan-btn");
+var elemWrapper = document.querySelector(".wrapper");
+var elemClose = document.querySelector(".modal-close")
+
+
+elmOpen.addEventListener("click",function(){
+    elemWrapper.classList.add("model-show")
+})
+
+elemClose.addEventListener("click",function(){
+    elemWrapper.classList.remove("model-show")
 })
 
